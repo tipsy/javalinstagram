b87ea94ea215313e5da28a8f11f6208f0b072ee5
David <davidaase@hotmail.com>
2018-03-03 13:59:57 +0100

    add dataclasses

---
+++ src/main/kotlin/DataClasses.kt
@@ -0,0 +1,7 @@
+import java.util.*
+
+data class User(val id: String, val password: String, val created: Date)
+data class Photo(val id: String, val ownerId: String, val likes: Int, val isLiked: Boolean, val created: Date)
+data class Like(val photoId: String, val ownerId: String)
+
+data class Credentials(val username: String = "", val password: String = "")
