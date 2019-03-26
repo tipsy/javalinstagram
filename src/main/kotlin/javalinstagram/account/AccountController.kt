b08c06001d76b0f4d246bc2ae1a7ae5aac2d856a
David <davidaase@hotmail.com>
2019-03-26 12:35:48 +0100

    Fix deprecated methods

--- src/main/kotlin/javalinstagram/account/AccountController.kt
+++ src/main/kotlin/javalinstagram/account/AccountController.kt
@@ -12,9 +12,9 @@ data class Credentials(val username: String = "", val password: String = "")
 object AccountController {
 
     fun signIn(ctx: Context) {
-        val (username, password) = ctx.validatedBody<Credentials>()
+        val (username, password) = ctx.bodyValidator<Credentials>()
                 .check({ it.username.isNotBlank() && it.password.isNotBlank() })
-                .getOrThrow()
+                .get()
         val user = AccountDao.findById(username)
         if (user != null && BCrypt.checkpw(password, user.password)) {
             ctx.status(200)
@@ -25,9 +25,9 @@ object AccountController {
     }
 
     fun signUp(ctx: Context) {
-        val (username, password) = ctx.validatedBody<Credentials>()
+        val (username, password) = ctx.bodyValidator<Credentials>()
                 .check({ it.username.isNotBlank() && it.password.isNotBlank() })
-                .getOrThrow()
+                .get()
         val user = AccountDao.findById(username)
         if (user == null) {
             AccountDao.add(id = username, password = BCrypt.hashpw(password, BCrypt.gensalt()))
--- src/main/kotlin/javalinstagram/like/LikeController.kt
+++ src/main/kotlin/javalinstagram/like/LikeController.kt
@@ -6,12 +6,12 @@ import javalinstagram.currentUser
 object LikeController {
 
     fun create(ctx: Context) {
-        LikeDao.add(photoId = ctx.validatedQueryParam("photo-id").getOrThrow(), ownerId = ctx.currentUser!!)
+        LikeDao.add(photoId = ctx.queryParam("photo-id")!!, ownerId = ctx.currentUser!!)
         ctx.status(201)
     }
 
     fun delete(ctx: Context) {
-        LikeDao.delete(photoId = ctx.validatedQueryParam("photo-id").getOrThrow(), ownerId = ctx.currentUser!!)
+        LikeDao.delete(photoId = ctx.queryParam("photo-id")!!, ownerId = ctx.currentUser!!)
         ctx.status(204)
     }
 
